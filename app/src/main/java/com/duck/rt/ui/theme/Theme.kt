e9ab3ac0c33586b7d4f613e0873c9e787429f4cf
JeeSeongDeok <bh939@naver.com>
2024-03-15 12:09:04 +0900

    [feature] add light color theme

--- app/src/main/java/com/duck/rt/ui/theme/Theme.kt
+++ app/src/main/java/com/duck/rt/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -22,9 +23,10 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF7F3DFF),
+    secondary = Color(0xFFF7F7F7),
+    tertiary = Color(0xFFEA6D7E),
+    background = Color(0xFFF3F2F8)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
